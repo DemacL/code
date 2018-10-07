fbc07658cddd67e3da666dcae42c679367a5e9b7
lizhicheng <245401566@qq.com>
2018-10-07 11:18:06 +0800

    换一台电脑测试提交

--- java/head-first-dp/src/observer/Main.java
+++ java/head-first-dp/src/observer/Main.java
@@ -9,9 +9,6 @@ public class Main {
         Observer1 o1 = new Observer1(wd);
         Observer2 o2 = new Observer2(wd);
         Observer3 o3 = new Observer3(wd);
-//        wd.registerObserver(o1);
-//        wd.registerObserver(o2);
-//        wd.registerObserver(o3);
 
 
         wd.notifyObservers();
