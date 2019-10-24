dfc5a930383ff90be1ad645a067261763131ee92
DemacL <245401566@qq.com>
2019-10-24 11:06:14 +0800

    快排

--- freecodecamp/quick-sort.js
+++ freecodecamp/quick-sort.js
@@ -44,3 +44,28 @@ function quickSort(arr) {
 }
 
 }
+
+//
+ts = Array.from({ length: 20 }, () => Math.floor(Math.random() * 100))
+function qs(arr) {
+    if (arr.length < 2) {
+        return arr;
+    } else {
+        let item = arr[0];
+        let left = [];
+        let right = [];
+        for (let i = 1; i < arr.length; i++) {
+            const num = arr[i];
+            if (num > item) {
+                right.push(num);
+            } else {
+                left.push(num);
+            }
+        }
+        return [...qs(left), item, ...qs(right)]
+    }
+
+}
+
+console.log(ts);
+console.log(qs(ts));
